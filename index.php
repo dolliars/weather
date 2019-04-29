edb70b5d1efadd640e5c8dee2edd665c871a3e14
dolliars <dolly.lanuza@gmail.com>
2019-04-29 14:23:06 -0400

    Initial state of old project

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+
+if ($_GET['city']) {
+	$_GET['city'] = str_replace(' ', '', $_GET['city']);
+	$forecastPage = file_get_contents("https://www.weather-forecast.com/locations/".$_GET['city']."/forecasts/latest");
+	$pageArray = explode('Day Weather Forecast Summary:</b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">', $forecastPage);
+	$secondPageArray = explode('</span></span></span>', $pageArray[1]);
+	$weather = $secondPageArray[0];
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+
+  <body>
+    <div class="container-fluid">
+      <div class="jumbotron" style="background-image: url(https://images.unsplash.com/photo-1496737018672-b1a6be2e949c?auto=format&fit=crop&w=2691&q=80)">
+        <h1 class="display-4">What's the weather?</h1>
+        <p class="lead">Enter name of a city</p>
+        <form>
+          <div class="form-group">
+	  <input type="text" class="form-control" name="city" id="city" placeholder="Eg: London, Montreal" style="width:450px" value="<?php echo $_GET['city']?>">
+          </div>
+          <p class="lead">
+            <button type="submit" class="btn btn-primary btn-lg">Submit</button>
+          </p>
+	</form>
+        <div id="weather"><?php
+          if ($weather) {
+		  echo '<div class="alert alert-success" role="alert">'.$weather.'</div>';
+	  }
+	?>
+        </div>
+
+    </div>
+
+    <!-- Optional JavaScript -->
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+  </body>
+</html>
