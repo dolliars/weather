39b55a3e72e39da29d5103eeef089a801ebe080b
dolliars <dolly.lanuza@gmail.com>
2019-04-29 22:54:28 -0400

    Strip user input and move php to include file
    We'll strip the user input of any special character and of any number
    input. Also move the php to the include file.

--- include.php
+++ include.php
@@ -1,4 +1,10 @@
 <?php
-echo "I'm an included file";
+if ($_GET['city']) {
+	$_GET['city'] = str_replace(' ', '', $_GET['city']);
+	$city = preg_replace("/[^a-zA-Z\s]/", "", $_GET['city']);
+	$forecastPage = file_get_contents("https://www.weather-forecast.com/locations/".$city."/forecasts/latest");
+	$pageArray = explode('<p class="b-forecast__table-description-content"><span class="phrase">', $forecastPage);
+	$secondPageArray = explode('</span></p>', $pageArray[1]);
+	$weather = $secondPageArray[0];
+}
 ?>
-
